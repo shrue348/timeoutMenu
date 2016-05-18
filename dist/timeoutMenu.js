c80b8f88636183f642c9ab08e5d94bec9c775987
shrue348 <shure348@gmail.com>
2016-05-18 11:30:09 +0300

    fixes

--- dist/timeoutMenu.js
+++ dist/timeoutMenu.js
@@ -1,69 +1,59 @@
-//timeoutMenu.js 0.1a https://github.com/shrue348/ Open source under the MIT License. Copyright © 2016 Aleksander Alekseev All rights reserved.
+//timeoutMenu.js 0.11a https://github.com/shrue348/ Open source under the MIT License. Copyright © 2016 Aleksander Alekseev All rights reserved.
 (function($) {
 	$.fn.timeoutMenu = function(options) {
 		var options = $.extend({
 			delay: 200
 		}, options),
+
 		
+		dragging = false,
 		doc = $(document),
 		body = $('body'),
 		obj = $(this),
-		ul = obj.find('ul'),
-		a_withsub = obj.find('ul').prev('a'),
-		ul_sub = obj.find('ul'),
-		
+		a = obj.find('a'),
+		ul = obj.find('a + ul'),
 		
 		init = function() {
-			window.globalMenuTimeout = false,
-			dragging = false;
+			window.globalMenuTimeout = false;
 			ul.hide();
-			a_withsub.addClass('test');
-
 			body.on('touchmove', function(){ dragging = true;});
 			body.on('', function(){	dragging = false;});
-			
-			doc.on('click', closemenu);
-			obj.on('mouseleave', closemenu);
-			a_withsub.on('mouseenter', opensub);
-			a_withsub.on('touchend focus', closesub);
+
+			a.on('mouseenter', a_hover);
+			a.on('touchend', sub_open);
+			obj.on('mouseleave', menu_close);
 		},
-		
-		closemenu = function(event){
-			if ($(event.target).closest(obj).length == 0 ) {
-					$(ul_sub).hide(); 
-					$(a_withsub).removeClass('tapped'); 
-				}
-			if ( event.type == 'mouseleave' ) {
-				window.clearTimeout(window.globalMenuTimeout);
-				window.globalMenuTimeout = window.setTimeout(function() {
-					$(ul_sub).hide(); 
-					$(a_withsub).removeClass('tapped'); 
-				}.bind(this), options.delay);
-			}
-		},
-		
-		opensub = function(){
+
+		a_hover = function(){
 			window.clearTimeout(window.globalMenuTimeout);
 			window.globalMenuTimeout = window.setTimeout(function() {
 				$(this).trigger('touchend');
 			}.bind(this), options.delay);
 		},
-		
-		closesub = function(e){
-			if(e.type == 'touchend' &&  dragging == false || e.type == 'mouseenter') {
-				if ( $(this).hasClass('tapped') ){
-					var href = $(this).attr('href');
-				} else {
-					$(this).parent().parent().find('.tapped').next('ul').hide();
-					$(this).parent().parent().find('.tapped').removeClass('tapped');
-					$(this).addClass('tapped');
-					$(this).next('ul').show();
-					e.stopImmediatePropagation();
-					e.preventDefault();
-				}
+
+		sub_open = function(e){
+			if ( $(this).hasClass('tapped') ){
+				var href = $(this).attr('href');
+				//return location.href(href);
+			} else {
+				$(this).parent().parent().find('.tapped').next('ul').hide();
+				$(this).parent().parent().find('.tapped').removeClass('tapped');
+
+				$(this).addClass('tapped');
+				$(this).next('ul').show();
+
+				e.stopImmediatePropagation();
+				e.preventDefault();
 			}
+		},
+		menu_close = function(){
+			window.clearTimeout(window.globalMenuTimeout);
+			window.globalMenuTimeout = window.setTimeout(function() {
+				obj.find('.tapped').next('ul').hide();
+				obj.find('.tapped').removeClass('tapped');
+			}, options.delay);
 		};
-		
+
 		return this.each(init);
 	}
 })(jQuery);
