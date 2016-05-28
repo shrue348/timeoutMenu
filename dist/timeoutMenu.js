d6ff9e634445408097619ddd74ec59015f2f9e4f
Aleksander Alekseev <shure348@gmail.com>
2016-05-28 19:00:48 +0300

    little minify js code
    remove waste lines

--- dist/timeoutMenu.js
+++ dist/timeoutMenu.js
@@ -1,11 +1,10 @@
-//timeoutMenu.js 0.11a https://github.com/shrue348/ Open source under the MIT License. Copyright © 2016 Aleksander Alekseev All rights reserved.
+//timeoutMenu.js 0.1a https://github.com/shrue348/ Open source under the MIT License. Copyright © 2016 Aleksander Alekseev All rights reserved.
 (function($) {
 	$.fn.timeoutMenu = function(options) {
 		var options = $.extend({
 			delay: 200
 		}, options),
-
-		
+	
 		dragging = false,
 		doc = $(document),
 		body = $('body'),
@@ -14,7 +13,7 @@
 		ul = obj.find('a + ul'),
 		
 		init = function() {
-			window.globalMenuTimeout = false;
+			globalMenuTimeout = false;
 			ul.hide();
 			body.on('touchmove', function(){ dragging = true;});
 			body.on('', function(){	dragging = false;});
@@ -25,8 +24,8 @@
 		},
 
 		a_hover = function(){
-			window.clearTimeout(window.globalMenuTimeout);
-			window.globalMenuTimeout = window.setTimeout(function() {
+			clearTimeout(globalMenuTimeout);
+			globalMenuTimeout = setTimeout(function() {
 				$(this).trigger('touchend');
 			}.bind(this), options.delay);
 		},
@@ -34,23 +33,18 @@
 		sub_open = function(e){
 			if ( $(this).hasClass('tapped') ){
 				var href = $(this).attr('href');
-				//return location.href(href);
 			} else {
-				$(this).parent().parent().find('.tapped').next('ul').hide();
-				$(this).parent().parent().find('.tapped').removeClass('tapped');
-
-				$(this).addClass('tapped');
-				$(this).next('ul').show();
+				$(this).parent().parent().find('.tapped').removeClass('tapped').next('ul').hide();
+				$(this).addClass('tapped').next('ul').show();
 
 				e.stopImmediatePropagation();
 				e.preventDefault();
 			}
 		},
 		menu_close = function(){
-			window.clearTimeout(window.globalMenuTimeout);
-			window.globalMenuTimeout = window.setTimeout(function() {
-				obj.find('.tapped').next('ul').hide();
-				obj.find('.tapped').removeClass('tapped');
+			clearTimeout(globalMenuTimeout);
+			globalMenuTimeout = setTimeout(function() 
+				obj.find('.tapped').removeClass('tapped').next('ul').hide();
 			}, options.delay);
 		};
 
